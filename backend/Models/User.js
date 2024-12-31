c085ca3966f956c9ce91c746bb0da0dc9e137091
sabbir ahammad <113548902+sabbirahammad@users.noreply.github.com>
2024-12-31 15:31:02 +0600

    Update User.js

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema; // Capital 'S' for Schema
+const Schema = mongoose.Schema; 
 
 const userSchema = new Schema({
     name: {
-        type: String, // Corrected 'typeof' to 'type'
+        type: String, 
         required: true
     },
     email: {
-        type: String, // Corrected 'typeof' to 'type'
+        type: String, 
         required: true,
         unique: true,
     },
     password: {
-        type: String, // Corrected 'typeof' to 'type'
+        type: String, 
         required: true
     }
 });
 
-const userModel = mongoose.model('User', userSchema); // Capitalized 'User' for model name
+const userModel = mongoose.model('User', userSchema); 
 module.exports = userModel;
