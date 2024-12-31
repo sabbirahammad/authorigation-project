64f4a18e3bd33f83bf49b57e06283d5bb05cad40
sabbir ahammad <113548902+sabbirahammad@users.noreply.github.com>
2024-12-31 15:32:34 +0600

    Update AuthValidation.js

--- backend/Middlewars/AuthValidation.js
+++ backend/Middlewars/AuthValidation.js
@@ -1,27 +1,27 @@
 const joi = require('joi');
 
 const signupValidation = (req, res, next) => {
-    // Define the validation schema
+   
     const schema = joi.object({
         name: joi.string().min(3).max(100).required(),
         email: joi.string().email().required(),
         password: joi.string().min(4).max(100).required(),
     });
 
-    // Validate the request body against the schema
-    const { error } = schema.validate(req.body, { abortEarly: false });  // Use abortEarly to show all errors at once
+ 
+    const { error } = schema.validate(req.body, { abortEarly: false });  
 
-    // If validation fails, return a 400 error with details
+
     if (error) {
         console.log(error.message);
         return res.status(400).json({
             message: 'Validation error',
             success: false,
-            details: error.details.map(err => err.message)  // Provide detailed error messages
+            details: error.details.map(err => err.message)  
         });
     }
 
-    // If validation passes, proceed to the next middleware
+
     next();
 };
 
