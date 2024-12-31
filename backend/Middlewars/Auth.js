3b8b18f11690b402927bd84f423f6920015db148
sabbir ahammad <113548902+sabbirahammad@users.noreply.github.com>
2024-12-31 15:33:02 +0600

    Update Auth.js

--- backend/Middlewars/Auth.js
+++ backend/Middlewars/Auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const ensureAuthenticated =(req, res, next) => {
-    // Extract Authorization header
+   
     const authHeader = req.headers["authorization"];
 
     if (!authHeader) {
@@ -18,16 +18,16 @@ const ensureAuthenticated =(req, res, next) => {
         // Verify the token using the same secret used when generating the token
         const decoded =jwt.verify(authHeader, process.env.JWT_TOKEN || "sabbirsammi");
 
-        // Log the decoded token for debugging
+      
         console.log("Decoded Token:", decoded);
 
-        // Store the decoded payload (user information) in req.user
+        
         req.user = decoded;
 
-        // Call the next middleware or route handler
+   
         next();
     } catch (error) {
-        // Log the error for debugging
+
         console.error("JWT Verification Error:", error);
 
         return res.status(403).json({
